84af9d9ac312a281934d97d6c6f90e860dbe86bb
Alejandra <aorozcoa1400@alumno.ipn.mx>
2021-12-01 20:10:36 -0600

    Solución de error al generar el Json

--- Js/CreateDoc.js
+++ Js/CreateDoc.js
@@ -1,10 +1,6 @@
 function crearJ(){
-    const myObjStr = JSON.stringify(shapes);
-    console.log(myObjStr);
-
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(shapes));
     var dlAnchorElem = document.getElementById('guardar');
     dlAnchorElem.setAttribute("href",     dataStr     );
     dlAnchorElem.setAttribute("download", "diagrama.json");
-    dlAnchorElem.click();
 }
