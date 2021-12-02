8e75519f6a33dc129aa932e52c5168f96a22c1bd
Alejandra <aorozcoa1400@alumno.ipn.mx>
2021-12-02 14:22:47 -0600

    Lee el archivo json y recupera el arreglo shapes

--- Js/CreateDoc.js
+++ Js/CreateDoc.js
@@ -1,6 +1,31 @@
+// Manejo  de JSON
+
+// Crea y  descarga y el archivo .json
 function crearJ(){
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(shapes));
     var dlAnchorElem = document.getElementById('guardar');
     dlAnchorElem.setAttribute("href",     dataStr     );
     dlAnchorElem.setAttribute("download", "diagrama.json");
+}
+
+//Lee el archivo .json
+function abrirArchivo (evento){
+    
+    let archivo = evento.target.files[0];
+    if (archivo) {
+        let reader = new FileReader();
+        reader.onload = function(e){
+            let contenido = e.target.result;
+            let json_data = JSON.parse(contenido);
+            for(var i in json_data){
+                alert('hola');
+                shapes.push([i, json_data [i]]);
+            }
+            
+            //reDraw();
+        };
+        reader.readAsText(archivo);
+    } else {
+        alert('No se subio el archivo exitosamente')
+    }
 }
