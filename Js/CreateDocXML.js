192a90d128bfce2fbd28a12f365ad4b01a321ab8
Alejandra <aorozcoa1400@alumno.ipn.mx>
2021-12-09 13:43:50 -0600

    Recupera el conector de la figura y dibuja el diagrama

--- Js/CreateDocXML.js
+++ Js/CreateDocXML.js
@@ -40,23 +40,18 @@ function recoveredSxml(xml_data){
     var cicle = xml_data.evaluate("/item/item/cicle|/item/item/parent::item", xml_data, null, XPathResult.ANY_TYPE, null);
     var resultCicle = cicle.iterateNext();
 
-    var start_shape = xml_data.getElementsByTagName("start_shape");
-    var end_shape = xml_data.getElementsByTagName("end_shape");
-    let cont  = 0;
-    let cont_if = 0;
-    let cont_while = 0;
     let a;
     while (result) {
         switch (result.childNodes[0].nodeValue) {
             case "start":
-                a = new Start(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new Start(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
                 shapes.push(a);
                 break;
             case "end":
-                a = new End(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new End(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
@@ -64,7 +59,7 @@ function recoveredSxml(xml_data){
                 shapes.push(a);
                 break;
             case "variable":
-                a = new Variable(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new Variable(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
@@ -72,7 +67,7 @@ function recoveredSxml(xml_data){
                 shapes.push(a);
                 break;
             case "input":
-                a = new DataInput(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new DataInput(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
@@ -80,7 +75,7 @@ function recoveredSxml(xml_data){
                 shapes.push(a);
                 break;
             case "output":
-                a = new DataOutput(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new DataOutput(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
@@ -88,7 +83,7 @@ function recoveredSxml(xml_data){
                 shapes.push(a);
                 break;
             case "assigment":
-                a = new Assignment(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new Assignment(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
 
@@ -97,7 +92,7 @@ function recoveredSxml(xml_data){
                 break;
             case "if":
                 cont_if ++;
-                a = new IfShape(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new IfShape(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;                
                 
@@ -105,7 +100,7 @@ function recoveredSxml(xml_data){
                 break;
             case "while":
                 cont_while++;
-                a = new WhileShape(resultX.childNodes[0].nodeValue, resultY.childNodes[0].nodeValue);
+                a = new WhileShape(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
                 a.text = resultText.childNodes[0].nodeValue;
                 a.bg_color = resultBg_color.childNodes[0].nodeValue;
                 a.trueValue = resulttv.childNodes[0].nodeValue;
@@ -115,9 +110,27 @@ function recoveredSxml(xml_data){
                 shapes.push(a);
                 break;
             default:
+                path = "/item/item/start_shape/item/type|/item/item/start_shape/item/parent::start_shape";
+                pathParte1 = "/item/item/start_shape/item/";
+                pathParte2 = "|/item/item/start_shape/item/parent::start_shape";
+                start_shape = recoverShape(xml_data, path, pathParte1, pathParte2);
+                path = "/item/item/end_shape/item/type|/item/item/end_shape/item/parent::end_shape";
+                pathParte1 = "/item/item/end_shape/item/";
+                pathParte2 = "|/item/item/end_shape/item/parent::end_shape";
+                end_shape = recoverShape(xml_data, path, pathParte1, pathParte2);
+                for(j in shapes){
+                    if(shapes[j].x  == start_shape.x && shapes[j].y == start_shape.y){
+                        for(k in shapes){
+                            if(shapes[k].x  == end_shape.x && shapes[k].y == end_shape.y){
+                                let a = new Link (shapes[j], shapes[k]);
+                                a.type = result.childNodes[0].nodeValue;
+                                connectors.push(a);
+                            }
+                        }
+                    }
+                }
                 break;
         }
-        cont++;
         result = nodes.iterateNext();
         resultX = x.iterateNext();
         resultY = y.iterateNext();
@@ -127,5 +140,78 @@ function recoveredSxml(xml_data){
         resultfv = falseValue.iterateNext();
         resultConnector = connector.iterateNext();
         resultCicle = cicle.iterateNext();
+    }
+}
+
+function recoverShape(xml_data, path, pathParte1, pathParte2){
+    var nodes = xml_data.evaluate(path, xml_data, null, XPathResult.ANY_TYPE, null);
+    var result = nodes.iterateNext();
+
+    var x = xml_data.evaluate(pathParte1+"x"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultX = x.iterateNext();
+    var y = xml_data.evaluate(pathParte1+"y"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultY = y.iterateNext();
+    var text = xml_data.evaluate(pathParte1+"text"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultText = text.iterateNext();
+    var bg_color = xml_data.evaluate(pathParte1+"bg_color"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultBg_color = bg_color.iterateNext();
+    var trueValue = xml_data.evaluate(pathParte1+"trueValue"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resulttv = trueValue.iterateNext();
+    var falseValue = xml_data.evaluate(pathParte1+"falseValue"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultfv = falseValue.iterateNext();
+    var connector = xml_data.evaluate(pathParte1+"connector"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultConnector = connector.iterateNext();
+    var cicle = xml_data.evaluate(pathParte1+"cicle"+pathParte2, xml_data, null, XPathResult.ANY_TYPE, null);
+    var resultCicle = cicle.iterateNext();
+
+    let a;
+        switch (result.childNodes[0].nodeValue) {
+            case "start":
+                a = new Start(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+
+                return a;
+            case "end":
+                a = new End(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                return a;
+            case "variable":
+                a = new Variable(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                return a;
+            case "input":
+                a = new DataInput(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                return a;
+            case "output":
+                a = new DataOutput(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                return a;
+            case "assigment":
+                a = new Assignment(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                return a;
+            case "if":
+                cont_if ++;
+                a = new IfShape(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;                
+                return a;
+            default :
+                cont_while++;
+                a = new WhileShape(parseInt(resultX.childNodes[0].nodeValue), parseInt(resultY.childNodes[0].nodeValue));
+                a.text = resultText.childNodes[0].nodeValue;
+                a.bg_color = resultBg_color.childNodes[0].nodeValue;
+                a.trueValue = resulttv.childNodes[0].nodeValue;
+                a.falseValue = resultfv.childNodes[0].nodeValue;
+                a.connector = resultConnector.childNodes[0].nodeValue;
+                a.cicle = resultCicle.childNodes[0].nodeValue;
+                return a;
     }
 }
